62af27867c6246bb0f452d0fd602b1d76395cbc0
Tim Mahy <timm@infosupport.com>
2017-06-19 18:00:28 +0200

    first setup

---
+++ TitanicML/Database/dbo/Tables/RawData.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[RawData] (
+    [PassengerId] VARCHAR (50)  NULL,
+    [Survived]    VARCHAR (50)  NULL,
+    [Pclass]      VARCHAR (255) NULL,
+    [Name]        VARCHAR (255) NULL,
+    [Sex]         VARCHAR (255) NULL,
+    [Age]         VARCHAR (255) NULL,
+    [SibSp]       VARCHAR (255) NULL,
+    [Parch]       VARCHAR (255) NULL,
+    [Ticket]      VARCHAR (255) NULL,
+    [Fare]        VARCHAR (255) NULL,
+    [Cabin]       VARCHAR (255) NULL,
+    [Embarked]    VARCHAR (255) NULL
+);
+
---
+++ TitanicML/Database/dbo/Views/STEP_1_TypedAndCleaned.sql
@@ -0,0 +1,30 @@
+﻿CREATE VIEW STEP_1_TypedAndCleaned
+AS
+SELECT TOP (1000) [PassengerId]
+      ,cast([Survived] as bit) as 'Survived'
+       ,(CASE 
+                  WHEN PClass = '1'
+                     THEN 'First'
+				  WHEN PClass = '2'
+					THEN 'Second'
+                  ELSE 'Third'
+				  END) as 'Class'
+      ,SUBSTRING([Name], 0,CHARINDEX(',',[Name])) as 'LastName'
+	  ,SUBSTRING( SUBSTRING([Name], CHARINDEX(',',[Name]) + 1,LEN([Name])),CHARINDEX('.',SUBSTRING([Name], CHARINDEX(',',[Name]) + 1,LEN([Name]))) + 1, LEN(SUBSTRING([Name], CHARINDEX(',',[Name]) + 1,LEN([Name])))) as 'FirstName'
+	  ,SUBSTRING(SUBSTRING([Name], CHARINDEX(',',[Name]) + 1,LEN([Name])),0,CHARINDEX('.',SUBSTRING([Name], CHARINDEX(',',[Name]) + 1,LEN([Name])))) as 'Title'
+      ,[Sex]
+      , (CASE
+		WHEN CHARINDEX('.',[Age]) > 0
+			THEN
+			 CAST(SUBSTRING([Age], 0,CHARINDEX('.',[Age])) as int)
+			ELSE
+			[Age]
+			END
+			) as 'Age'
+      ,cast([SibSp] as int) as 'NumberOfSiblings'
+      ,cast([Parch] as int) as 'NumberOfParentsChildren'
+      ,[Ticket]
+      ,cast([Fare] as decimal(18,2)) as 'Fare'
+      ,[Cabin]
+      ,[Embarked]
+  FROM [dbo].[RawData]
